cf338e43720c8c447a84a88172873e217005cb9c
Marshal Horn <kamocat@gmail.com>
2022-12-26 23:25:10 -0800

    Set up systemd timers

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # olcc
 Monitoring Oregon liquor prices
+
+##Directories
+
+- *csv* is for holding the price logs
+- *systemd* has an example systemd timer script so you can fetch the data monthly
--- fetch_liquor.py
+++ fetch_liquor.py
@@ -1,8 +1,10 @@
+#!/usr/bin/python
 import requests
 import datetime as dt
-
+import os 
+dir_path = os.path.dirname(os.path.realpath(__file__))
 t = dt.datetime.today()
-fname = f'csv/{t.year}_{t.month}.csv'
+fname = f'{dir_path}/csv/{t.year}_{t.month}.csv'
 url = "https://www.olcc.state.or.us/pdfs/NumericPriceListCurrentMonth.csv"
 response = requests.get(url)
 if response.status_code == 200:
