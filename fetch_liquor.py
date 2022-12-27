0a24803074ac58d8207f5a282eee313eb770215b
Marshal <kamocat@gmail.com>
2022-12-27 07:46:17 -0800

    Fixed path formatting. Now uses modified date instead of fetched date.

--- fetch_liquor.py
+++ fetch_liquor.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python
 import requests
-import datetime as dt
-import os 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-t = dt.datetime.today()
-fname = f'{dir_path}/csv/{t.year}_{t.month}.csv'
+from datetime import datetime
+from os import path
+
+fname = path.dirname(path.realpath(__file__))
+fname = path.join(fname, 'csv')
+
 url = "https://www.olcc.state.or.us/pdfs/NumericPriceListCurrentMonth.csv"
 response = requests.get(url)
 if response.status_code == 200:
-    with open(fname, 'wb') as file:
-        file.write(response.content)
+    date = response.headers['last-modified']
+    # example date Thu, 01 Dec 2022 13:26:02 GMT
+    t = datetime.strptime(date, '%a, %d %b %Y %H:%M:%S %Z')
+    fname = path.join(fname, t.strftime('%Y_%m.csv'))
+    with open(fname, 'w') as file:
+        file.write(f"Modified {date}\r\n")
+        file.write(response.text)
 else:
     print('Failed to download OLCC price list. Status code:', response.status_code)
 
