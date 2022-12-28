1f70d220b493e9a39beddf6606a7a12ca3bca5a2
Marshal <kamocat@gmail.com>
2022-12-28 12:32:43 -0800

    Creates categories, but bottles table is empty

--- aggregate.py
+++ aggregate.py
@@ -7,7 +7,8 @@ cur = con.cursor()
 #For testing, we're overwriting the data
 cur.execute('DROP TABLE IF EXISTS bottles');
 cur.execute('DROP TABLE IF EXISTS prices');
-cur.execute('CREATE TABLE bottles(item INTEGER PRIMARY KEY,status,description NOT NULL,size,age,proof REAL,case_size INTEGER)')
+
+cur.execute('CREATE TABLE bottles(item INTEGER PRIMARY KEY,status,description NOT NULL,size,age,proof REAL,case_size INTEGER, category INTEGER)')
 cur.execute('CREATE TABLE prices(item INTEGER, price REAL, date)')
 
 mydir = os.path.dirname(os.path.realpath(__file__))
@@ -22,14 +23,23 @@ for f in filenames:
         date = next(csvreader)
         header = next(csvreader)
         for row in csvreader:
-            try:
-                cur.execute('INSERT INTO bottles VALUES (?,?,?,?,?,?,?)', row[1:8])
-            except:
-                pass # It's ok if the entry is already there
+            cur.execute('INSERT INTO bottles VALUES (?,?,?,?,?,?,?)', row[1:8])
             cur.execute('INSERT INTO prices VALUES (?,?,?)', [row[1],row[8],row[9]])
 
 
         con.commit()
+
+import categories as cat
+cur.execute('CREATE TABLE IF NOT EXISTS categories(id INTEGER PRIMARY KEY, name NOT NULL)')
+for i, val in enumerate(cat.cat):
+    cur.execute('INSERT OR IGNORE INTO categories VALUES (?,?)', [i,val])
+con.commit()
+bottles = cat.extract('categories.txt')
+for x in bottles:
+    cur.execute('UPDATE bottles SET category = ? WHERE item = ?', [bottles[x], x])
+con.commit()
+
+
 con.close()
 
     
